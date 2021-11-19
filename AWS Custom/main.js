fe2f728982a39fcf61434dc379168a1943827dc8
Wellington Santana <90020289+Welldevbr@users.noreply.github.com>
2021-11-19 13:12:36 -0300

    Aba About
    Implementada a aba de sobre nós.

--- AWS Custom/main.js
+++ AWS Custom/main.js
@@ -1,8 +1,20 @@
+// Open and Close: Menu hamburguer
+
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
+  })
+}
+
+// Close Click intem
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
