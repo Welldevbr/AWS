474891717d5d0913f920ba6b019999197278c11d
Wellington Santana <90020289+Welldevbr@users.noreply.github.com>
2021-11-22 00:44:39 -0300

    Mobile Completo
    Finalizada toda a parte mobile.

--- AWS Custom/main.js
+++ AWS Custom/main.js
@@ -21,16 +21,16 @@ for (const link of links) {
 
 // change page header when scrolling
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderPage() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 // Feedbacks slider
 
@@ -53,12 +53,34 @@ const scrollReveal = ScrollReveal({
 })
 
 scrollReveal.reveal(
-  `
-#home .image, #home .text,
+  `#home .image, #home .text,
 #about .image, #about .text,
 #services header, #services .card,
 #feedbacks header, #feedbacks .testimonials,
-#contact .text, #contact .links
+#contact .text, #contact .links,
+footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+// Back to top
+
+const backToTopButton = document.querySelector('.back-to-top')
+const animationClass = 'show'
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  const animationClass = 'show'
+
+  if (scrollY >= 1000) {
+    backToTopButton.classList.add(animationClass)
+  } else {
+    backToTopButton.classList.remove(animationClass)
+  }
+}
+
+// Scroll events
+
+window.addEventListener('scroll', function () {
+  backToTop(), changeHeaderPage()
+})
